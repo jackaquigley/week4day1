51b2cc46803328ba47c9e0322cda24e7891d0c37
Jack Quigley <jackaquigley@gmail.com>
2020-03-30 19:09:47 +0100

    Initial Commit, webpage returns correct result for game

---
+++ controller.rb
@@ -0,0 +1,41 @@
+require ('sinatra')
+require ('sinatra/contrib/all') if development?
+
+require_relative('models/rockpaperscissors.rb')
+also_reload('./models/*')
+
+get '/rock/rock' do
+  return "Draw."
+end
+
+get 'paper/paper' do
+  return "Draw."
+end
+
+get 'scissors/scissors' do
+  return "Draw."
+end
+
+get 'rock/paper' do
+  return "Paper wins."
+end
+
+get 'rock/scissors' do
+  return "Rock wins."
+end
+
+get 'paper/rock' do
+  return "Paper wins."
+end
+
+get 'paper/scissors' do
+  return "Scissors wins."
+end
+
+get 'scissors/rock' do
+  return "Rock wins."
+end
+
+get 'scissors/paper' do
+  return "Paper wins."
+end
